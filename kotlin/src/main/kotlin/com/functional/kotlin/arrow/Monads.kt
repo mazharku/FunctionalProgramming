21ee1dec25e0a84c8d51f80a4c854d6975932b44
mazharibnazahur <mazhar.zahur@dohatec.com.bd>
2023-02-27 23:45:50 +0600

    update

--- kotlin/src/main/kotlin/com/functional/kotlin/arrow/Monads.kt
+++ kotlin/src/main/kotlin/com/functional/kotlin/arrow/Monads.kt
@@ -1,4 +1,12 @@
 package com.functional.kotlin.arrow
 
-class Monads {
+import arrow.core.Either
+
+fun divide(num1: Int, num2: Int): Either<Throwable, Int> {
+    return try {
+        val res = num1 / num2;
+        Either.Right(res);
+    } catch (e: Exception) {
+        Either.Left(e)
+    }
 }
--- kotlin/src/test/kotlin/com/functional/kotlin/arrow/ArrowTest.kt
+++ kotlin/src/test/kotlin/com/functional/kotlin/arrow/ArrowTest.kt
@@ -1,4 +1,20 @@
 package com.functional.kotlin.arrow
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class ArrowTest {
+
+    @Test
+    fun divideShouldReturnResultForValidInput() {
+        val result = divide(4, 2);
+        val expected = 2;
+        Assertions.assertEquals(expected, result.let { it.orNull() });
+    }
+
+    @Test
+    fun diveShouldReturnErrorMessageWhenInvalidInput() {
+        val result = divide(4, 0);
+        Assertions.assertTrue(result.isLeft())
+    }
 }
